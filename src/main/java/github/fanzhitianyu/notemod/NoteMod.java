bbf08148b6b616494b63fb8e1d8d239aad3b33fa
Fanzhitianyu <fanzhiyuxian@163.com>
2023-04-20 23:34:17 +0800

    first

--- src/main/java/github/fanzhitianyu/notemod/NoteMod.java
+++ src/main/java/github/fanzhitianyu/notemod/NoteMod.java
@@ -1,4 +1,10 @@
 package github.fanzhitianyu.notemod;
+import net.fabricmc.api.ModInitializer;
+public class NoteMod implements ModInitializer
+{
+    @Override
+    public void onInitialize()
+    {
 
-public class NoteMod {
+    }
 }
